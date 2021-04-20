866f4e8ae28aa4743c92b8114a790d0db745386c
Your Name <you@example.com>
2021-04-20 19:44:46 +0300

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.default')
+
+@section('content')
+
+    @if($posts->count())
+        @foreach($posts as $post)
+            <article>
+                <h2>{{$post->title}}</h2>
+                <p>{{Str::limit($post->body, 50)}}</p>
+                <a href="{{route('get-post', ['slug' => $post->slug])}}">Чиатать далее...</a>
+            </article>
+        @endforeach
+    @endif
+    @stop
